c8128b316bfa5145f5f91d8f8ebbbf02ab09005f
aferrero2707 <aferrero1975@gmail.com>
2017-05-30 15:09:10 +0200

    Update

--- main.cc
+++ main.cc
@@ -1,7 +1,14 @@
 #include<iostream>
+#include <stdio.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+
 
 int main(int argc, char** argv)
 {
+  xmlDocPtr doc; /* the resulting document tree */
+
+  doc = xmlReadFile("file.xml", NULL, 0);
 	std::cout<<"Test"<<std::endl;
 	return 0;
 }
